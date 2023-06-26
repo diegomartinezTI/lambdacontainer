11aa042b4db65d1f91a78fa716c7593e38e33f2b
diegomartinezTI <diegomp1407@gmail.com>
2023-06-26 12:19:58 -0600

    fix

--- example/lambda_function.py
+++ example/lambda_function.py
@@ -9,10 +9,12 @@ import botocore
 ssm = boto3.client("ssm")
 session = boto3.Session( aws_access_key_id='AKIAQJZNGEWMWYXNQ3GX', aws_secret_access_key='M9OVWwswPXQ1HSOZRSEtoamTYY8Q03gpg+qQxwOf')
 s3 = session.resource('s3')
-KEY_NAME = ssm.get_parameter(Name="keyname")["Parameter"]["Value"]
-EMAIL = ssm.get_parameter(Name="encryptemail")["Parameter"]["Value"]
+
+
  
 def handler(event, context):
+    KEY_NAME = ssm.get_parameter(Name="keyname")["Parameter"]["Value"]
+    EMAIL = ssm.get_parameter(Name="encryptemail")["Parameter"]["Value"]
     my_bucket = s3.Bucket('merchantkey')
     lst = os.listdir("/tmp")
     print(lst) 
@@ -22,31 +24,50 @@ def handler(event, context):
     file = my_bucket.download_file("texto.txt", "/tmp/texto.txt") 
     lst = os.listdir("/tmp")
     print(lst) 
-    result = encrypt_file("/tmp/texto.txt") 
+    result = encrypt_file("/tmp/texto.txt",EMAIL,KEY_NAME) 
         
   
     
     return f'Gpg encrypt ok'       
 
 
-def encrypt_file(file_name):
+def encrypt_file(file_name,email,key_name): 
     gpg_homeshort = "/tmp"
     gpg = gnupg.GPG(gnupghome=gpg_homeshort, verbose=True)
-    key = open(f"/tmp/{KEY_NAME}", "rb").read()
+    key = open(f"/tmp/{key_name}", "rb").read()
     gpg.import_keys(key)
-    gpg.list_keys() 
+    gpg.list_keys()
+    name_encripted = file_name.split("/")[-1].split(".")
     with open(file_name, "rb") as f:
-        status = gpg.encrypt_file(
+        gpg.encrypt_file(
             f,
-            recipients=[EMAIL],
-            output=f"/tmp/{file_name}.gpg",
+            recipients=[email],
+            output=f"/tmp/{name_encripted[0]}.gpg",
             always_trust=True,
             extra_args=["--yes"],
         )
-        print(status) 
-    lst = os.listdir("/tmp")
+        lst = f"{name_encripted[0]}.gpg"
+
+        return lst
+
+# def encrypt_file(file_name):
+#     gpg_homeshort = "/tmp"
+#     gpg = gnupg.GPG(gnupghome=gpg_homeshort, verbose=True)
+#     key = open(f"/tmp/{KEY_NAME}", "rb").read()
+#     gpg.import_keys(key)
+#     gpg.list_keys() 
+#     with open(file_name, "rb") as f:
+#         status = gpg.encrypt_file(
+#             f,
+#             recipients=[EMAIL],
+#             output=f"/tmp/{file_name}.gpg",
+#             always_trust=True,
+#             extra_args=["--yes"],
+#         )
+#         print(status) 
+#     lst = os.listdir("/tmp")
     
-    return lst
+#     return lst
 
 
  
